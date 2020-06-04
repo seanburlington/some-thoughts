461b3b5f1f67474df6df9226e897eca2574d7290
Sean Burlington <sean@tangiblebytes.co.uk>
2020-06-04 16:28:06 +0100

    ensure build is prod one

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-hugo
+HUGO_ENV="production" hugo
 aws --profile burlington s3 cp ./public/ s3://seanburlington --recursive
 aws --profile burlington cloudfront create-invalidation --distribution-id EY7NM35CR9EM5 --paths '/'
 aws --profile burlington cloudfront create-invalidation --distribution-id EY7NM35CR9EM5 --paths '/*'
